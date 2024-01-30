0177276b6f364c349ae31fb56ab24c15203640de
Lilglu4e <mohammadsarrafmstr2000@gmail.com>
2024-01-30 14:49:31 -0500

    state change...

--- front_end.py
+++ front_end.py
@@ -1,25 +1,34 @@
 import streamlit as st
 # Import your converted script or functions here
-from test import UF_get_cell_value, UFcomms, UFtypes,UFR_get_cell_value, UFRcomms, UFRtypes
+from test import UF_get_cell_value, UFcomms, UFtypes, UFR_get_cell_value, UFRcomms, UFRtypes
 
-st.title("3SB Edge solver")
+# Initialize a session state if not already done
+if 'solver_type' not in st.session_state:
+    st.session_state['solver_type'] = '3SB Corner solver'
 
-# Use your notebook functions as needed
-# input_str = 'mx ow ea ju rf np'.replace(" ", "")
-input_str = st.text_input("Enter the memorization...")
-input_str = input_str.replace(" ", "")
+# Title with button to change the state
+st.write("# 3 Style Blind Solver", unsafe_allow_html=True)
+if st.button('Change Solver'):
+    st.session_state['solver_type'] = '3SB Edge solver' if st.session_state['solver_type'] == '3SB Corner solver' else '3SB Corner solver'
 
-result = UF_get_cell_value(input_str, UFtypes, UFcomms)
-print(result)
-st.write(result)
+app_mode = st.session_state['solver_type']
 
-st.title("3SB Corner solver")
+if app_mode == "3SB Corner solver":
+    st.header("Corner Solver")
 
-# Use your notebook functions as needed
-# input_str = 'mx ow ea ju rf np'.replace(" ", "")
-input_str = st.text_input("Enter the memorization for the corner...")
-input_str = input_str.replace(" ", "")
+    # Use your notebook functions as needed
+    input_str = st.text_input("Enter the memorization for the corner...")
+    input_str = input_str.replace(" ", "")
 
-result = UFR_get_cell_value(input_str, UFRtypes, UFRcomms)
-print(result)
-st.write(result)
+    result = UFR_get_cell_value(input_str, UFRtypes, UFRcomms)
+    st.write(result)
+
+elif app_mode == "3SB Edge solver":
+    st.header("Edge Solver")
+
+    # Use your notebook functions as needed
+    input_str = st.text_input("Enter the memorization for the edge...")
+    input_str = input_str.replace(" ", "")
+
+    result = UF_get_cell_value(input_str, UFtypes, UFcomms)
+    st.write(result)
